5223780fd31e572e0994b06551e217fd91967d43
thunderthumbz <ryanmacdwork@gmail.com>
2026-01-03 14:40:07 +0100

    Update vite.config.ts
    Update base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
-  base: "/talintel-website/", // <--- Important for GitHub Pages
+  base: "/", // <--- Changed from "/talintel-website/" to "/" for custom domain
   plugins: [
     react(),
     runtimeErrorOverlay(),
